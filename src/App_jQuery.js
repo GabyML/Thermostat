e9982355a12167470c54d8e7634862a2e41009b1
GabyML <gabriela.lagunes.11@uclac.uk>
2015-08-19 18:07:00 +0100

    jQuery AJAX call for current weather running

--- src/App_jQuery.js
+++ src/App_jQuery.js
@@ -1,18 +1,34 @@
-thermostat = new Thermostat();
-
-var temperature = $('span')[0];
-ShowTemperature = function() {
-  temperature.innerHTML = thermostat.degrees;
-};
-TemperatureColor = function() {
-temperature.style.color = thermostat.colorControl();
-};
-
 $( document ).ready(function(){
-  $('.temperature').show(function(){
+
+	thermostat = new Thermostat();
+
+	var temperature = $('span')[0];
+	ShowTemperature = function() {
+		temperature.innerHTML = thermostat.degrees;
+	};
+	TemperatureColor = function() {
+		temperature.style.color = thermostat.colorControl();
+	};
+
+	$(".see_weather").click(function(event){
+		getWeather();
+	}); // links html file with processed info
+
+	function getWeather(){
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric", function(info) {
+			showWeather(info);
+		})
+	}; // gets info from server
+
+	function showWeather(weather_info){
+		$(".weather").text(weather_info.weather[0].description);
+		$(".weather_temp").text(Math.round(weather_info.main.temp) + "°C");
+		var weather_image_src = "http://openweathermap.org/img/w/" + weather_info.weather[0].icon + ".png";
+		$(".weather_image").html("<img src=" + weather_image_src + ">");
+	};// process the info gotten from server to send it to the browser
+
   	ShowTemperature();
   	TemperatureColor();
-  })
 
    $('button').eq(0).click(function(){
 	thermostat.increase();
